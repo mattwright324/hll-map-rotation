6b85e0ad3336aa14ff0aae9741c2593d576ff194
Matthew Wright <mattwright324@gmail.com>
2023-05-18 08:12:38 -0400

    add stress flag and check for consecutive stress maps

--- README.md
+++ README.md
@@ -5,17 +5,22 @@ Generate random weighted map rotations for the hll_rcon_tool
 ## Usage
 
 ```sh
-$ ./new_rotation.py -c "4w 1-2o"
-$ ./new_rotation.py -c "3-5w 1-2o 1-2w 1o"
-$ ./new_rotation.py -nc "8a"
+$ ./new_rotation.py -cs "5w 1o 5w 1o"
+$ ./new_rotation.py -cs "7-8wd 1wn 2og"
+$ ./new_rotation.py -cs "11w"
+$ ./new_rotation.py -ncs "8a"
 ```
 
 ### Options
 
-- `-w` or  `--weight` generate the list with weight. enabled by default
 - `-n` or  `--no-weight` generate the list with no weight.
-- `-d` or `--allow-dupes` allow duplicates in the list. disabled by default
+- `-d` or `--allow-dupes` allow exact duplicates in the list. disabled by default
+    - Example: two *carentan_warfare* in the list
+- `-g` or `--disable-general-dupes` allow general duplicates in the list. enabled by default
+    - Example: two *carentan* of any type in the list
 - `-x` or `--allow-consecutive` allow consecutive of the same general map. disabled by default
+    - Example: *foy_warfare* followed by *foy_warfare_night*
+	- Dependent on allowed duplicate options
 - `-c "<config>"` or  `--config "<config>"` advanced specify generated config by number/range/type
     - `#` static amount
 	- `#-#` range amount
@@ -26,3 +31,5 @@ $ ./new_rotation.py -nc "8a"
 	- `o` offensive mode. includes both variants by default
 	    - `g` axis attacking variant
 		- `u` allies attacking variant
+- `-s` or `--seed` generate ideal seeding rotation based on the generated rotation
+    - Tries to make sure duplicates/consecutives do not happen or are far enough apart when changing from seeding to live rotations.
--- new_rotation.py
+++ new_rotation.py
@@ -48,38 +48,83 @@ def readConfig(configStr):
     
     return config
 
+def generateSeedRotation(data, full_rotation):
+    seed_maps = []
+    for row in data:
+        if str(row["seeding"]).upper() == "TRUE":
+            seed_maps.append(row["map"])
+
+    # Populate blanks
+    seed_rotation = []
+    for i in range(0, min(len(seed_maps), len(full_rotation)), 1):
+        seed_rotation.append("")
+
+    # Match index
+    for i in range(0, len(seed_maps), 1):
+        if i > len(full_rotation):
+            break;
+        if full_rotation[i] in seed_maps:
+            seed_rotation[i] = full_rotation[i]
+            seed_maps.remove(seed_rotation[i])
+
+    # Fill in blanks by distance for dupes outside seed rotation length
+    dist_tuples = []
+    for map in seed_maps:
+        if map in full_rotation:
+            full_ind = full_rotation.index(map)
+            dist_tuples.append((full_ind, map))
+    dist_tuples.sort()
+    for tuple in dist_tuples:
+        # print(tuple, " dist ", tuple[0] - seed_rotation.index(""))
+        seed_rotation[seed_rotation.index("")] = tuple[1]
+        seed_maps.remove(tuple[1])
+
+    # Fill in remaining blanks if they exist
+    for i in range(0, len(seed_rotation), 1):
+        if seed_rotation[i] == "" and len(seed_maps) > 0:
+            seed_rotation[i] = seed_maps[0]
+            seed_maps.remove(seed_maps[0])
+
+    return seed_rotation
+
 def main(argv):
     weighted = True
-    duplicates = False
-    consecutive = False
-    config = '3-5w 1-2o 2w 1o'
+    exact_dupes = False
+    general_dupes = False
+    seeding = False
+    config = '11w'
+    consec_stress = False
 
-    opts, args = getopt.getopt(argv, "hwdxnc:", ["weight", "allow-dupes", "allow-consecutive", "no-weight", "config="])
+    opts, args = getopt.getopt(argv, "hndgxnc:s", ["no-weight", "allow-dupes", "disable-general-dupes", "general-dupe-dist=", "seed", "config="])
     for opt, arg in opts:
         if opt == '-h':
             print('new_rotation.py --weight --config "<configs>"')
             sys.exit()
-        elif opt in ("-w", "--weight"):
-            weighted = True
         elif opt in ("-n", "--no-weight"):
             weighted = False
-        elif opt in ("-d", "--allow-dupes"):
-            duplicates = True
-        elif opt in ("-x", "--allow-consecutive"):
-            consecutive = True
+        elif opt in ("-d", "--allow-exact-dupes"):
+            exact_dupes = True
+        elif opt in ("-x", "--disable-general-dupes"):
+            general_dupes = False
+        elif opt in ("-p", "--general-dupe-dist"):
+            general_dupe_dist = arg
+        elif opt in ("-s", "--seed"):
+            seeding = True
         elif opt in ("-c", "--config"):
-           config = arg
-    # print('Using weight : ', weighted)
-    # print('Allow dupes  : ', duplicates)
+            config = arg
     configs = config.split(" ")
-    # print('Configs      : ', configs)
     
+    stress_maps = []
     data = []
     with open("hll_rcon_maps.csv", "r") as file:
         reader = csv.reader(file);
         headers = next(reader);
         data = [{h:x for (h,x) in zip(headers,row)} for row in reader]
-    
+
+    for row in data:
+        if str(row["stress"]).upper() == "TRUE":
+            stress_maps.append(row["map"])
+
     full_rotation = [];
     # print()
     for configStr in configs:
@@ -104,25 +149,61 @@ def main(argv):
                     map_weights.append(float(50))
         amount = random.randint(config.range[0], config.range[1])
         
-        if not duplicates:
+        if not exact_dupes:
             amount = min(len(map_options), amount)
+
+        def check_good_result(result):
+            good_result = True
+            length = len(full_rotation)
+
+            if not exact_dupes and result in full_rotation:
+                good_result = False
+            if not general_dupes: # and result.split("_")[0] == full_rotation[len(full_rotation)-1].split("_")[0]
+                for j in range(0, length, 1):
+                    if result.split("_")[0] == full_rotation[j].split("_")[0]:
+                        good_result = False
+            if not consec_stress:
+                if result in stress_maps and length > 0 and full_rotation[length-1] in stress_maps:
+                    good_result = False
+                    
+            return good_result
 
         for i in range(0, amount, 1):
             while True:
                 result = random.choices(map_options, weights=map_weights, k=1)[0];
-
-                good_result = True
-                if not duplicates and result in full_rotation:
-                    good_result = False
-                if not consecutive and len(full_rotation) > 0 and result.split("_")[0] == full_rotation[len(full_rotation)-1].split("_")[0]:
-                    good_result = False
+                good_result = check_good_result(result)
+                
+                any_good_results = False
+                for map in map_options:
+                    if check_good_result(map):
+                        any_good_results = True
                 
                 if good_result:
                     full_rotation.append(result)
                     break
+                if not any_good_results:
+                    # print("No more good results possible, stopping early.")
+                    break
     
     # print('Generation done full rotation:')
-    print(full_rotation)
+    # print(full_rotation)
+
+    print("### Live Rotation ###")
+    print('"rotation": [')
+    print("  \"","\",\n  \"".join(full_rotation),"\"", sep='')
+    print("]")
+    print()
+    print()
+
+    if seeding:
+        seed_rotation = generateSeedRotation(data, full_rotation);
+        
+        print("### Seed Rotation ###")
+        print('"rotation": [')
+        print("  \"","\",\n  \"".join(seed_rotation),"\"", sep='')
+        print("]")
+        print()
+        print()
 
 if __name__ == "__main__":
     main(sys.argv[1:])
